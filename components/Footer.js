630b791ef95f59402b4294586e9eb82ece908f58
bwyard <bwyard@gmail.com>
2024-01-15 18:37:21 -0600

    add copyright

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-          <p>Ariel Pierce</p>
+          <p> &copy; Ariel Pierce</p>
       </footer>
     </>
   )
